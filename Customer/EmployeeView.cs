2d23230e58b28bb46958b62e42a5377d950dc268
ycfung306 <ycfung306@gmail.com>
2025-05-19 10:20:14 +0800

    make template

--- Customer/EmployeeView.cs
+++ Customer/EmployeeView.cs
@@ -6,62 +6,6 @@ namespace Project
 {
     public partial class EmployeeView : Form
     {
-        private EmployeeRepo employeeRepo = new EmployeeRepo();
-        private double temp = 1;
-        public EmployeeView()
-        {
-            InitializeComponent();
-            refreshDataGrid();
-        }
-
-        private void txtName_TextChanged(object sender, EventArgs e)
-        {
-            refreshDataGrid();
-        }
-
-        public void refreshDataGrid()
-        {
-
-            List<Employee> employees = employeeRepo.GetByName(txtName.Text);
-            dgvEmployee.Columns.Clear();
-            dgvEmployee.DataSource = null;
-
-            dgvEmployee.AutoGenerateColumns = false;
-            dgvEmployee.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Employee Number", DataPropertyName = "employeeNumber", Name = "employeeNumber" });
-            dgvEmployee.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Name", DataPropertyName = "fullName", Name = "fullName" });
-            dgvEmployee.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Title", DataPropertyName = "jobTitle", Name = "jobTitle" });
-
-            dgvEmployee.DataSource = employees;
-        }
-
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            AddEmployeeForm form = new AddEmployeeForm(this);
-            form.Show();
-        }
-
-        private void btnView_Click(object sender, EventArgs e)
-        {
-            if (dgvEmployee.SelectedCells.Count > 0)
-            {
-                int selectedRowIndex = dgvEmployee.SelectedCells[0].RowIndex;
-                DataGridViewRow selectedRow = dgvEmployee.Rows[selectedRowIndex];
-                int employeeNum = Convert.ToInt32(selectedRow.Cells["employeeNumber"].Value.ToString());
-                if (employeeRepo.GetById(employeeNum) != null) {
-                    EmployeeDetailsView view = new EmployeeDetailsView(employeeNum);
-                    view.Show();
-                }
-                else
-                {
-                    MessageBox.Show("Employee not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    refreshDataGrid();
-                }
-            }
-            else
-            {
-                MessageBox.Show("Please select a employee to view.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            
-        }
+        
     }
 }
--- Customer/Program.cs
+++ Customer/Program.cs
@@ -6,17 +6,6 @@ namespace Project
 {
     static class Program
     {
-
-        public static MySqlConnection GetConnection()
-        {
-            string host = Properties.Resources.host;
-            string db = Properties.Resources.db;
-            string userId = Properties.Resources.userId;
-            string password = Properties.Resources.password;
-            string connStr = $"Server={host};Database={db};User ID={userId};Password={password};Pooling=true;";
-
-            return new MySqlConnection(connStr);
-        }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
